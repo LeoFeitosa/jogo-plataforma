13e4b0bfed25957e982437eb80d24a2ccb83c599
Leo <leofeitosa@live.com>
2019-02-23 18:16:15 -0300

    Pulo e Animação do pulo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,20 +14,48 @@ public class Player : MonoBehaviour
 
     private bool jumping;
 
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, radiusCheck, layerGround);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            //Comandos do pulo
+            jumping = true;
+        }
+
+        PlayAnimations();
     }
 
     private void FixedUpdate()
     {
-        
+        if (jumping)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jumping = false;
+        }   
+    }
+
+    void PlayAnimations()
+    {
+        if (grounded && rb2d.velocity.x == 0)
+        {
+            anim.Play("Idle");
+        }
+        else if (!grounded)
+        {
+            anim.Play("Jump");
+        }
     }
 }

23da790342227bde96c068d2b4e20f7c01b87034
Leo <leofeitosa@live.com>
2019-04-06 20:46:06 -0300

    Efeito paralax

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public Transform[] bgs;
+    public float[] paralaxVel;
+    public float smooth;
+    public Transform cam;
+
+    private Vector3 previewCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previewCam = cam.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0; i<bgs.Length; i++)
+        {
+            float paralax = (previewCam.x - cam.position.x) * paralaxVel [i];
+            float targetPosX = bgs[i].position.x - paralax;
+            Vector3 targetPos = new Vector3(targetPosX, bgs[i].position.y, bgs[i].position.y);
+            bgs[i].position = Vector3.Lerp(bgs[i].position, targetPos, smooth * Time.deltaTime);
+        }
+        previewCam = cam.position;
+    }
+}

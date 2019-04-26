a37a98bc1fccf5ce563f7f67f4db6ae9a125aaff
Leo <leofeitosa@live.com>
2019-04-25 23:53:31 -0300

    GameManager inicial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    public static GameManagement instance;
+    public Sprite[] overlaySprites;
+    public Image overlay;
+    public Text timeHud;
+    public Text scoreHud;
+
+    private float time;
+    private int score;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

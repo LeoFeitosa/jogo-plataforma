4346799b8c518ebc54151d2972b7e8d379624ee8
Leo <leofeitosa@live.com>
2019-04-26 22:38:03 -0300

    Contador de tempo e score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,11 @@ public class GameManagement : MonoBehaviour
     public Text timeHud;
     public Text scoreHud;
 
-    private float time;
-    private int score;
+    public float time;
+    public int score;
+
+    public enum GameStatus { WIN, LOSE, DIE, PLAY }
+    public GameStatus status;
 
     void Awake()
     {
@@ -29,12 +32,28 @@ public class GameManagement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 30f;
+        score = 0;
+        status = GameStatus.PLAY;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(status == GameStatus.PLAY)
+        {
+            time -= Time.deltaTime;
+            int timeInt = (int)time;
+
+            if(timeInt >= 0)
+            {
+                timeHud.text = "Time: " + timeInt.ToString();
+                scoreHud.text = "Score: " + score.ToString();
+            }
+
+        } else
+        {
+            // exibe os overlays
+        }
     }
 }

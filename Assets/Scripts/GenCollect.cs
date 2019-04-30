1729fe500e1d752ea38a8c2c8a7b7fc022358e47
Leo <leofeitosa@live.com>
2019-04-29 22:33:18 -0300

    Contando pontos na coleta de diamantes

--- Assets/Scripts/GenCollect.cs
+++ Assets/Scripts/GenCollect.cs
@@ -7,6 +7,7 @@ public class GenCollect : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) {
+            GameManager.instance.score++;
             Destroy(gameObject);
         }
     }
